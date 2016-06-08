56b6092bd39a83612d0cf4ca085b56d2d1642e65
shark-S <sunilsangwan0098@gmail.com>
2016-06-08 13:05:29 +0530

    working vacss

--- main.js
+++
@@ -1,7 +0,0 @@
-var express = require('express')
-console.log("Check check");
-var webRTC = require('webrtc.io').listen(8001);
-var app = express();
-app.get('*',function(req,res){
-	res.sendfile('index.html');
-})
---
+++ server.js
@@ -0,0 +1,34 @@
+var websocket = require('ws').Server;
+var http = require('http')
+var express = require('express')
+var app = express()
+var port = process.env.PORT || 5000
+
+app.use(express.static(__dirname+"/"))
+
+var server = http.createServer(app)
+server.listen(port)
+
+console.log("http server is listening on %d",port)
+
+var wss = new websocket({server:server})
+console.log("websocket server created")
+
+wss.broadcast = function (data){
+		var i=0,n=this.clients?this.clients.length:0,client=null;
+		for(;i<n;i++){
+console.log("Clinet "+i+" Here");
+			client = this.clients[i];
+			if(client.readyState === client.OPEN){
+				client.send(data);
+			}
+			else console.error('Error:the client state is '+client.readyState);
+
+		}
+	};
+	wss.on('connection',function(ws){
+		ws.on('message',function(message){
+			wss.broadcast(message);
+			console.log("websocket connection is open")
+		});
+	});
