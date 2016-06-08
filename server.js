baba87b20caa10cc918dae3ece680ea86c8a580d
shark-S <sunilsangwan0098@gmail.com>
2016-06-08 14:53:59 +0530

    ping server check

--- server.js
+++ server.js
@@ -27,6 +27,18 @@ console.log("Clinet "+i+" Here");
 		}
 	};
 	wss.on('connection',function(ws){
+		
+  var id = setInterval(function() {
+    ws.send(JSON.stringify(new Date()), function() {  })
+  }, 1000)
+
+  console.log("websocket connection open")
+
+  ws.on("close", function() {
+    console.log("websocket connection close")
+    clearInterval(id)
+  })
+})
 		ws.on('message',function(message){
 			wss.broadcast(message);
 			console.log("websocket connection is open")
